de0343fb008bf3532c44e507213785529c921830
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-05-21 18:49:33 -0700

    Make bezier a class

--- lightbulb/moveable_point.js
+++ lightbulb/moveable_point.js
@@ -22,5 +22,25 @@ function InteractiveBezier(a1x, a1y, c1x, c1y, c2x, c2y, a2x, a2y) {
   this.a2 = new MoveablePoint(a2x, a2y);
   this.c2 = new MoveablePoint(c2x, c2y);
 
+  this.draw = function() {
+    bezier(this.a1.x, this.a1.y, this.c1.x, this.c1.y, this.c2.x, this.c2.y, this.a2.x, this.a2.y);
+    line(this.c1.x, this.c1.y, this.a1.x, this.a1.y);
+    this.c1.draw();
+    line(this.c2.x, this.c2.y, this.a2.x, this.a2.y);
+    this.c2.draw();
+  }
 
+  this.checkPoints = function() {
+    if (this.c1.mouseOver()) {
+      this.c1.x = mouseX;
+      this.c1.y = mouseY;
+    } else if (this.c2.mouseOver()) {
+      this.c2.x = mouseX;
+      this.c2.y = mouseY;
+    }
+  }
+
+  this.getPoints = function() {
+    return [this.a1.x, this.a1.y, this.c1.x, this.c1.y, this.c2.x, this.c2.y, this.a2.x, this.a2.y];
+  }
 }
