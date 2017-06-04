1a1c0e1483d4506ddd536088bd2ba224dfabbfc2
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-06-04 00:53:35 -0400

    Add color generation

--- mta_subway_brochure/sketch.js
+++ mta_subway_brochure/sketch.js
@@ -6,16 +6,35 @@ var lineSpacing = canvasHeight / 100;
 var lineLengthMin = canvasWidth * 0.05;
 var lineLengthMax = canvasWidth * 0.75;
 
+var USE_COLORS = true;
+var NUM_COLORS = 9;
+var colors = [];
+
+function fillHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+function strokeHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   background(backgroundColor);
   strokeWeight(lineHeight);
+
+  // fill color palette
+  for (i = 0; i < NUM_COLORS; i++) {
+    colors[i] = [int(random(37)) * 10, 100, 35 + 25 / NUM_COLORS * i];
+  }
 }
 
 function draw() {
   currentY = lineSpacing + lineHeight / 2;
   canvasBounds = [0, canvasWidth];
-  while (currentY < canvasHeight - (lineHeight + lineSpacing)) {
+  while (currentY < canvgiasHeight - (lineHeight + lineSpacing)) {
     pointA = canvasBounds[int(random(1, 3)) - 1];
     pointB = new int;
     lineLength = random(lineLengthMin, lineLengthMax);
@@ -27,7 +46,12 @@ function draw() {
         pointB = canvasWidth - lineLength;
         break;
     }
-    stroke(lineColor);
+
+    if (USE_COLORS) {
+      chosenColor = colors[int(random(colors.length))];
+      console.log(chosenColor);
+      strokeHsluv.apply(null, chosenColor);
+    } else stroke(lineColor);
     line(pointA, currentY, pointB, currentY);
     currentY += lineSpacing + lineHeight;
     console.log(pointA + ', ' + lineLength + ': ' + pointB);
